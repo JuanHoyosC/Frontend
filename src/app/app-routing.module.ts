731c494367e293076394d7f18ebe823e5418d2c2
Juan Carlos Hoyos <jcabarique@uninorte.edu.co>
2020-04-23 13:28:22 -0500

    Se comenzo a diseñar el front de las tareas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//Components
+import { PrivateTaskComponent } from './components/private-task/private-task.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { TaskComponent } from './components/task/task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/task', pathMatch: 'full'},
+  {path: 'task', component: TaskComponent},
+  {path: 'task-private', component: PrivateTaskComponent },
+  {path: 'signup', component: SignupComponent},
+  {path: 'signin', component: SigninComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { TaskComponent } from './components/task/task.component';
+import { PrivateTaskComponent } from './components/private-task/private-task.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    TaskComponent,
+    PrivateTaskComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
